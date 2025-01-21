019208536839db98abf8a25e79e8ae48e6ece257
DevJorgeRafael <rafaelrosero.r.r2408@gmail.com>
2025-01-21 09:28:34 -0500

    config routes and db.json

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,30 +1,42 @@
 import { Routes } from '@angular/router';
-import { InicioComponent } from './inicio/inicio.component';
-import { AboutComponent } from './about/about.component';
-import { ConsultaComponent } from './consulta/consulta.component';
-import { CreacionComponent } from './creacion/creacion.component';
-import { EdicionComponent } from './edicion/edicion.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { AboutComponent } from './components/about/about.component';
+import { ConsultaComponent } from './components/empleados/consulta/consulta.component'; 
+import { CreacionComponent } from './components/empleados/creacion/creacion.component'; 
+import { EdicionComponent } from './components/empleados/edicion/edicion.component'; 
+import { AlcoholesComponent } from './components/alcoholes/alcoholes-list/alcoholes.component';
+import { CreacionAlcoholComponent } from './components/alcoholes/creacion/creacion.component';
+import { EdicionAlcoholComponent } from './components/alcoholes/edicion/edicion.component';
 
 export const routes: Routes = [
     {
         path: 'inicio',
         component: InicioComponent
-    }, 
-    {
+    },{
         path: 'about',
         component: AboutComponent
-    }, 
-    {
-        path: 'consulta',
-        component: ConsultaComponent
     },
+
     {
-        path: 'creacion',
+        path: 'empleados',
+        component: ConsultaComponent,
+    },{
+        path: 'empleados/creacion',
         component: CreacionComponent
-    }, 
-    {
-        path: 'edicion/:id',
+    },{
+        path: 'empleados/edicion/:id',
         component: EdicionComponent
+    },
+
+    {
+        path: 'alcoholes',
+        component: AlcoholesComponent,
+    },{
+        path: 'alcoholes/creacion',
+        component: CreacionAlcoholComponent
+    },{
+        path: 'alcoholes/edicion/:id',
+        component: EdicionAlcoholComponent
     }
 
 ];
