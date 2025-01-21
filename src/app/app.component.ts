dd3f2d4e8b47078c5ca3fd8f555f5f866281ca07
DevJorgeRafael <rafaelrosero.r.r2408@gmail.com>
2025-01-20 22:47:00 -0500

    config app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { AboutComponent } from './about/about.component';
+import { ConsultaComponent } from './consulta/consulta.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, InicioComponent, AboutComponent, ConsultaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'empleados-app';
+  title = 'Empleados App';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { AboutComponent } from './about/about.component';
+import { ConsultaComponent } from './consulta/consulta.component';
+import { CreacionComponent } from './creacion/creacion.component';
+import { EdicionComponent } from './edicion/edicion.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'inicio',
+        component: InicioComponent
+    }, 
+    {
+        path: 'about',
+        component: AboutComponent
+    }, 
+    {
+        path: 'consulta',
+        component: ConsultaComponent
+    },
+    {
+        path: 'creacion',
+        component: CreacionComponent
+    }, 
+    {
+        path: 'edicion/:id',
+        component: EdicionComponent
+    }
+
+];
